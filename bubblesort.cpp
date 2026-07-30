5f6ded1e0f1794e45ac43221307accdf81819ee1
Mokshana1229 <mokshana2006@gmail.com>
2026-07-30 15:41:19 +0530

    bubblesort.cpp

---
+++ bubblesort.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    bool swapped;
+    
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+    
+        for (int j = 0; j < n - i - 1; j++) {
+            // Swap if the element found is greater than the next element
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = true; 
+            }
+        }
+        
+        If no elements were swapped in the inner loop, break
+             if (!swapped) {
+             break;
+        }
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]); // Calculate array size
+    
+    cout << "Original array: ";
+    printArray(arr, n);
+    
+    bubbleSort(arr, n);
+    
+    cout << "Sorted array:   ";
+    printArray(arr, n);
+    
+    return 0;
+}
